a3f5dc3f6dbae5184c959fd916a58ec6e3921f7a
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-03-30 08:07:10 +0545

    overall functionality setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,65 @@
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./context";
 
 export default function App() {
-  
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodo) => [{ id: Date.now(), ...todo }, ...prevTodo]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => {
+        prevTodo.id === id ? todo : prevTodo;
+      })
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) =>
+      prev.filter((prevTodo) => {
+        prevTodo.id !== id;
+      })
+    );
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem(todos));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <h1>Todo Site with chai aur code.</h1>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
