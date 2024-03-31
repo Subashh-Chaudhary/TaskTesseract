0772e80f6d69e0a69c268762bb07c10d0c567a81
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-03-31 16:00:15 +0545

    todoform, todoitem components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
+import { TodoForm, TodoItem } from "./components";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
@@ -54,9 +55,17 @@ export default function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
